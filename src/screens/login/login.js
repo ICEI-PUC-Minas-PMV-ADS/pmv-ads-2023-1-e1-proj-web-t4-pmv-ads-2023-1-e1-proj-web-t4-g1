f91bd0f98ef88f94c29415ae7e1cfeb99b98b7cc
Eddy <mouragestao@gmail.com>
2023-05-14 08:48:28 -0300

    Cria funcionalidade cadastro

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -1,5 +1,46 @@
+const signUpForm = document.querySelector(".signup form");
+const emailInput = signUpForm.querySelector(".email");
+const nameInput = signUpForm.querySelector(".cpf");
+const passwordInput = signUpForm.querySelector(".password");
 showOrHidePassword = document.querySelectorAll(".eye-icon");
 links = document.querySelectorAll(".link");
+
+
+signUpForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const email = emailInput.value;
+    const name = nameInput.value;
+    const password = passwordInput.value;
+
+    const isValid = validateFormData(email, name, password);
+
+    if (isValid) {
+        alert("Cadastro realizado com sucesso!");
+        signUpForm.reset();
+    } else {
+        alert("Erro ao criar conta. Tente novamente mais tarde.");
+    }
+
+})
+
+function validateFormData(email, name, password) {
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    if (!emailRegex.test(email)) {
+        return false;
+    }
+
+    if (name.length < 3) {
+        return false;
+    }
+
+    if (password.length < 6) {
+        return false;
+    }
+
+    return true;
+}
+
 
 showOrHidePassword.forEach(eyeIcon => {
     eyeIcon.addEventListener("click", () => {
