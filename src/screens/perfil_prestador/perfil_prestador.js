74a8d3f49d710bd1fdecbe6ee61ae67a98d695c2
Naianna <naianna.sdc@gmail.com>
2023-05-14 16:58:31 +0100

    Construção da tela de perfil do prestador do serviço

---
+++ src/screens/perfil_prestador/perfil_prestador.js
@@ -0,0 +1,8 @@
+document.getElementById("btn-contato").onclick = function () {
+    // location.href = "http://google.com";
+    alert("Contato: XXXX-XXXX");
+};
+document.getElementById("btn-msg").onclick = function () {
+    // location.href = "http://google.com";
+    alert("Caixa de Mensagem Indisponível!!!");
+};
