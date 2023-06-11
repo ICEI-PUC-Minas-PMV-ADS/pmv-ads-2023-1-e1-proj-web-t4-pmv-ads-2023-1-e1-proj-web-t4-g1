651e4df9b7d921e6342f86729bbc68049f7b7031
Naianna <naianna.sdc@gmail.com>
2023-06-11 20:41:11 +0100

    Atualização do Perfil do Prestador. Inclusão da Caixa de mensagem e links das redes sociais

--- src/screens/perfil_prestador/perfil_prestador.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("btn-contato").onclick = function () {
-    // location.href = "http://google.com";
-    alert("Contato: XXXX-XXXX");
-};
-document.getElementById("btn-msg").onclick = function () {
-    // location.href = "http://google.com";
-    alert("Caixa de Mensagem Indisponível!!!");
-};
